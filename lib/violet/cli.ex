fb86b76591ddb6e74d97c71e5419ca2b4a413442
lucas.andrade6 <lucas.andrade@ilia.digital>
2023-03-24 01:43:21 -0300

    refactor: adjustments in CLI code

--- lib/violet/cli.ex
+++ lib/violet/cli.ex
@@ -6,23 +6,15 @@ defmodule Commandline.CLI do
     read_file(opts[:unitTest])
   end
 
-  def read_file(path) do
-    case File.read(path) do
-      {:ok, content} ->
-        IO.puts("Lendo arquivo no diretório...")
-        compile_svelte_file(path, content)
-      {:error, reason} ->
-        IO.puts("Erro ao ler o arquivo: #{reason}")
-        ""
-    end
-  end
-
   def write_file(path, content) do
-    js_filename = "#{Path.basename(path, ".svelte")}.spec.js"
-    js_dirname = Path.dirname(path)
+    js_generated_file = "#{Path.dirname(path)}/#{Path.basename(path, ".svelte")}.spec.js"
+    File.touch(js_generated_file, System.os_time(:second))
 
-    try do File.write("#{js_dirname}/#{js_filename}", content)
-      {:ok, IO.puts("Arquivo #{js_filename} salvo em #{path}")}
+    try do
+      File.write(js_generated_file, content)
+
+      {:ok,
+       IO.puts("Arquivo #{Path.basename(path, ".svelte")}.spec.js salvo em #{Path.dirname(path)}")}
     catch
       :enoent -> {:error, "Diretório não encontrado."}
       :eacces -> {:error, "Permissão negada."}
@@ -30,25 +22,49 @@ defmodule Commandline.CLI do
       :eexist -> {:error, "Já existe um arquivo com este nome."}
       :badarg -> {:error, "Argumento inválido."}
     end
-
   end
 
-  def compile_svelte_file(path, file) do
-    {:ok, response} =
-      OpenAI.completions(
-        model: "text-davinci-003",
-        prompt: "create unit test in jest #{file}",
-        max_tokens: 2048,
-        temperature: 0.1
-      )
-
-    text =
-      response
+  def get_content_file(path, content) do
+    choice =
+      content
       |> Map.get(:choices)
       |> hd()
 
+    write_file(path, choice["text"])
+  end
 
-      write_file(path, text["text"])
-    response
+  def compile_svelte_file(path, file) do
+    case(
+      OpenAI.completions(
+        model: "text-davinci-003",
+        prompt:
+          "create unit test in jest importing and using this svelte component called #{Path.basename(path)} #{file}",
+        max_tokens: 2048,
+        temperature: 0,
+        n: 1,
+        best_of: 1
+      )
+    ) do
+      {:ok, content} ->
+        IO.puts("Gravando arquivo de testes...")
+        get_content_file(path, content)
+
+      {:error, reason} ->
+        {:error, "Failed to read file: #{reason}"}
+    end
+  end
+
+  def read_file(path) do
+    case File.read(path) do
+      {:ok, content} ->
+        IO.puts("Lendo arquivo no diretório...")
+        compile_svelte_file(path, content)
+
+      {:error, :enoent} ->
+        {:error, "File not found: #{path}"}
+
+      {:error, reason} ->
+        {:error, "Failed to read file: #{reason}"}
+    end
   end
 end
